45f35bc55620c71d198ebc80548504808aa5918e
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-02-17 19:56:43 -0800

    Save work.

--- src/images/github-brands.js
+++ src/images/github-brands.js
@@ -9,11 +9,10 @@ const GitHub = () => {
       focusable="false"
       data-prefix="fab"
       data-icon="github"
-      class="svg-inline--fa fa-github fa-w-16"
       role="img"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 496 512"
-      style={{
+      sx={{
         height: '30px',
         fill: 'inherit'
       }}
